14849f903220bd4b9957f4629e7193cbc650758c
GeoMukkath <geomukkath@yandex.com>
2021-12-27 12:22:51 +0530

    Added color button with changing color functionality

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+
+  const randomRgb = () => {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+  };
   return (
     <View>
-      <Button title="Add color" />
+      <Button
+        title="Add color"
+        onPress={() => setColors([...colors, randomRgb()])}
+      />
+      <View style={{ width: 100, height: 100, backgroundColor: randomRgb() }} />
     </View>
   );
 };
