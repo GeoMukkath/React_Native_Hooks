2c36a7b03f39be9ac383fed744e4d51bb365b5b3
GeoMukkath <geomukkath@yandex.com>
2021-12-27 11:57:46 +0530

    Started with colors screen

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const ColorScreen = () => {
+  return (
+    <View>
+      <Button title="Add color" />
+    </View>
+  );
+};
+
+const View = styled.View``;
+const Text = styled.Text``;
+const Button = styled.Button``;
+
+export default ColorScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -18,6 +18,10 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Counter"
         onPress={() => navigation.navigate("Counter")}
       />
+      <Button
+        title="Go to Color Screen"
+        onPress={() => navigation.navigate("Color")}
+      />
     </View>
   );
 };
