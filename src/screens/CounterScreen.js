ff0d996b225a4c74b0ad405f0558cf8364c0b600
GeoMukkath <geomukkath@yandex.com>
2021-12-27 11:32:33 +0530

    Finished Counter Screen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-native";
 import styled from "styled-components";
 
 const CounterScreen = () => {
-  return <Text>Counter: </Text>;
+  const [count, setCount] = useState(0);
+  return (
+    <View>
+      <Button title="Increase Count" onPress={() => setCount(count + 1)} />
+      <Button title="Decrease Count" onPress={() => setCount(count + -1)} />
+      <Text>Counter: {count}</Text>
+    </View>
+  );
 };
 
 const Text = styled.Text``;
+const View = styled.View``;
 
 export default CounterScreen;
