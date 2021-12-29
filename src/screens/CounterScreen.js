3a5e500e84b8e63edb33b8bb035c609c77df7126
GeoMukkath <geomukkath@yandex.com>
2021-12-29 10:46:18 +0530

    Refactored Counter App with useReducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,14 +1,31 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { Button } from "react-native";
 import styled from "styled-components";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "inc":
+      return state + action.payload;
+    case "dec":
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
+
 const CounterScreen = () => {
-  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, 0);
   return (
     <View>
-      <Button title="Increase Count" onPress={() => setCount(count + 1)} />
-      <Button title="Decrease Count" onPress={() => setCount(count + -1)} />
-      <Text>Counter: {count}</Text>
+      <Button
+        title="Increase Count"
+        onPress={() => dispatch({ type: "inc", payload: 1 })}
+      />
+      <Button
+        title="Decrease Count"
+        onPress={() => dispatch({ type: "dec", payload: 1 })}
+      />
+      <Text>Counter: {state}</Text>
     </View>
   );
 };
