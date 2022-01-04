710f40ef700dbc3ac7eb6a02bf938835afd35eba
GeoMukkath <geomukkath@yandex.com>
2022-01-04 09:50:44 +0530

    Added Yelp search api via axios

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,32 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import yelp from "../screens/api/yelp";
 import SearchBar from "./components/SearchBar";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: "san jose",
+      },
+    });
+    setResults(response.data.businesses);
+  };
+
   return (
     <View>
       <Text>Search Screen</Text>
       <SearchBar
         term={term}
         onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log("Test Submitted")}
+        onTermSubmit={() => searchApi()}
       />
-      <Text>{term}</Text>
+      <Text>We have found {results.length} results for your search term.</Text>
     </View>
   );
 };
---
+++ src/screens/api/yelp.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization:
+      "Bearer YHrmVW_hkn1tcvHFLjFgylel60zo5HjfCHEXQPXWGUariQnzvNksXJJKCA66_Bw4ELA80zmZm4gOjDrDGvCUZDTvrtE349bK-WwmPQeZysavFKYs601Ch38aGZLOYXYx",
+  },
+});
